886cab59c3ef01ff1a474043db3303c64375e8e5
mbaker <mekenna.drabner@gmail.com>
2024-07-27 15:27:25 -0600

    downloaded inquirer, created question array

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 // TODO: Include packages needed for this application
 
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'How do you use this project?',
+    },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: 'List any collaborators here.',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license does your project have?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -12,3 +45,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
