d288e5bdced4f0e2423992080bae0ced5fc0b1a0
mbaker <mekenna.drabner@gmail.com>
2024-07-28 13:50:04 -0600

    added fs to needed packages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 
-import inquirer from 'inquirer';
+import inquirer from 'inquirer'; //for the prompts
+import fs from 'fs'; //to write file
 
 // TODO: Create an array of questions for user input
 
@@ -17,32 +18,50 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'tableOfcontents',
+        message: 'Provide the Table of Contents (optional)',
+    },
+    {
+        type: 'input',
         name: 'installation',
         message: 'What are the installation instructions?',
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: 'How do you use this project?',
     },
     {
         type: 'input',
-        name: 'Credits',
-        message: 'List any collaborators here.',
+        name: 'contributing',
+        message: 'What are the contributing guidelines',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
     },
     {
         type: 'input',
         name: 'license',
         message: 'What license does your project have?',
     },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Provide your contact information for questions.',
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log(`${fileName} has been created!`)
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {};
 // Function call to initialize app
 init();
 
